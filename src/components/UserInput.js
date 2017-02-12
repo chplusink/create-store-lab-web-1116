88cb8e8b3ad2de08138a3e093f7487ee5938c29b
Vic Lam <lam.vic.k@gmail.com>
2017-02-11 23:40:25 -0500

    Add add user capabilities

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,13 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class UserInput extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      userName: '',
+      hometown: ''
+    }
+  }
+
+  handleSubmit(event){
+    event.preventDefault()
+    this.props.store.dispatch({type: 'ADD_USER', payload: this.state})
+  }
+
+  userNameChange(event){
+    this.setState({userName: event.target.value})
+  }
+
+  hometownChange(event){
+    this.setState({hometown: event.target.value})
+  }
+
   render(){
     return(
       <div>
-        user input
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input type="text" value={this.state.userName} onChange={this.userNameChange.bind(this)}/>
+          <input type="text" value={this.state.hometown} onChange={this.hometownChange.bind(this)}/>
+          <input type="submit"/>
+        </form>
       </div>
     )
   }
+
 }
+
+
 
 export default UserInput;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
 
 import { createStore } from 'redux'
+
+import App from './App'
 import manageBand from './reducers/manageBand'
 
 // we wrap store in a function for testing purposes
 export function configureStore(){
+  return createStore(manageBand)
 }
 
-
-
+const store = configureStore()
+store.dispatch({type: 'ROLLOUT'})
 
 ReactDOM.render(
-  <App store={configureStore()} />,
+  <App store={store} />,
   document.getElementById('root')
 );
